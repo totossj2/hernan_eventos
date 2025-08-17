b2e9d98ba3bdda21e6fa690c4378e05a4b706edb
totossj2 <loferrario@uade.edu.ar>
2025-08-17 20:22:29 -0300

    update

--- lib/constants.ts
+++ lib/constants.ts
@@ -72,7 +72,7 @@ export const CLIENTS = [
   },
   {
     name: 'Municipalidad de Esteban Echeverría',
-    logo: '/logos_clientes/MEE.png', // TODO: cambiar a la nueva imagen
+    logo: '/logos_clientes/mee.png',
     alt: 'Logo Municipalidad de Esteban Echeverría',
   },
   {
