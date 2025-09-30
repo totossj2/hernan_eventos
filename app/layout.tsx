9ca9e4469ee79e72da08c923b8a699fed687eee2
totossj2 <loferrario@uade.edu.ar>
2025-09-30 17:13:51 -0300

    updated favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -42,6 +42,7 @@ export const metadata: Metadata = {
       { url: '/favicon.ico', sizes: 'any' },
       { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
       { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+      { url: '/favicon-48x48.png', sizes: '48x48', type: 'image/png' },
     ],
     apple: [
       { url: '/apple-touch-icon.png', sizes: '180x180', type: 'image/png' },
@@ -109,6 +110,12 @@ export default function RootLayout({
           href="/favicon-32x32.png"
           type="image/png"
           sizes="32x32"
+        />
+        <link
+          rel="icon"
+          href="/favicon-48x48.png"
+          type="image/png"
+          sizes="48x48"
         />
         <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
         <link rel="manifest" href="/site.webmanifest" />
