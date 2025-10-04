b1649dd9fffeda9ae3cd948bd0b1d204ff81e264
totossj2 <loferrario@uade.edu.ar>
2025-10-04 17:18:02 -0300

    esconder nav

--- lib/constants.ts
+++ lib/constants.ts
@@ -10,8 +10,8 @@ export const CONTACT = {
 export const NAVIGATION = [
   { name: 'Inicio', href: '/' },
   { name: 'Servicios', href: '/servicios' },
-  { name: 'Trabajos', href: '/trabajos' },
-  { name: 'Quienes Somos', href: '/quienes_somos' },
+  //{ name: 'Trabajos', href: '/trabajos' },
+  //{ name: 'Quienes Somos', href: '/quienes_somos' },
 ];
 
 export const SERVICES = [
